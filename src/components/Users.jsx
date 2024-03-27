4e2ae08ba3fb9b3b5256e29346c4b29592c105b0
Peter Osoo <posoo617@gmail.com>
2024-03-27 18:31:08 +0300

    fetch users

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,6 +1,34 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
 const Users = () => {
+	const [users, setUsers] = useState([])
 	const url = "https://jsonplaceholder.typicode.com/users"
-	return <div>Users</div>
+
+	const fetchUsers = () => {
+		return axios.get(url).then(res => setUsers(res.data))
+	}
+
+	useEffect(() => {
+		fetchUsers()
+
+		return () => {
+			console.log(users)
+		}
+	}, [])
+
+	return (
+		<div>
+			<h2>Users </h2>
+			{users.map((user, index) => {
+				return (
+					<div key={index}>
+						<li>{user.name}</li>
+					</div>
+				)
+			})}
+		</div>
+	)
 }
 
 export default Users
